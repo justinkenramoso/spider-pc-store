c906fc6a8d70615d2f5014e61a0c0a434af6c0de
justinkenramoso <justinken.ramoso@gmail.com>
2022-12-06 07:44:24 +0800

    Added products and categorization functionality.

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -74,18 +74,29 @@ exports.all = (req, res) => {
   }).lean();
 };
 
+// Categorize
+exports.categorize = (req, res) => {
+  ProductsModel.find({ category: req.query.category }, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(data);
+      res.render("products", { data });
+    }
+  }).lean();
+};
+
 // Insert
 exports.insert = (req, res) => {
   const productsModel = new ProductsModel();
   productsModel.product_id = Date.now();
-  productsModel.name =
-    "Gigabyte B450 Aouros Pro Wifi with 4+2 Phase IR Digital PWM, Intel Dual Band 802.11ac Socket Am4 Ddr4 Gaming Motherboard";
-  productsModel.price = "₱8,450.00";
+  productsModel.name = "Seagate 1tb ST1000DM010 Harddisk Drive";
+  productsModel.price = "₱2,159.00";
   productsModel.image =
-    "https://cdn.shopify.com/s/files/1/0101/4864/2879/products/1218-a_540x.jpg?v=1587045645";
+    "https://cdn.shopify.com/s/files/1/0101/4864/2879/products/160-a_540x.jpg?v=1635836886";
   productsModel.specs =
-    "<ul><li>Brand: GIGABYTE</li><li>Model: B450 AORUS PRO WIFI (rev. 1.0)</li><li>CPU Socket Type: AM4</li><li>CPU Type: Supports AMD 3rd Gen Ryzen/2nd Gen Ryzen/1st Gen Ryzen / 2nd Gen Ryzen with Radeon Vega Graphics/1st Gen Ryzen with Radeon Vega Graphics / Athlon with Radeon Vega Graphics Processors</li><li>Chipset: AMD B450</li><li>Number of Memory Slots: 4×288pin</li><li>Memory Standard: Support for DDR4 3600(O.C.)/ 3466(O.C.)/ 3200(O.C.)/ 2933/ 2667/ 2400/ 2133 MHz memory modules</li><li>Maximum Memory Supported: 128GB</li><li>Channel Supported: Dual Channel</li><li>PCI Express 3.0 x16: 1 x PCI Express x16 slot, running at x16 (PCIEX16)</li><li>PCI Express 2.0 x16: 1 x PCI Express x16 slot, running at x4 (PCIEX4), 1 x PCI Express x16 slot, running at x1 (PCIEX1_2)</li><li>PCI Express x1: 1 x PCI Express x1 slot (PCIEX1_1)</li><li>Other Slots: 1 x M.2 Socket 1 connector for a wireless communication module (M2_WI-FI)</li><li>SATA 6Gb/s: 6 x SATA 6Gb/s</li><li>M.2: 1 x M.2 connector (Socket 3, M key, type 2242/2260/2280/22110 SATA and PCIe 3.0 x4/x2 SSD support) (M2A_SOCKET) 1 x M.2 connector (Socket 3, M key, type 2242/2260/2280 PCIe 3.0 x2 SSD support) (M2B_SOCKET)</li><li>Audio Chipset: Realtek ALC1220-VB codec</li><li>Audio Channels: 2/4/5.1/7.1-channel Support for S/PDIF Out</li><li>LAN Chipset: Intel GbE LAN chip</li><li>Max LAN Speed: 10/100/1000Mbps</li><li>Wireless LAN: Wi-Fi 802.11 a/b/g/n/ac, supporting 2.4/5 GHz Dual-Band</li><li>Support for 11 ac wireless standard and up to 433 Mbps data rate</li><li>Bluetooth: BLUETOOTH 4.2</li><li>Back I/O Ports:</li><li>1 x HDMI port</li><li>2 x SMA antenna connectors (1T1R)</li><li>1 x DVI-D port</li><li>4 x USB 3.1 Gen 1 ports</li><li>1 x USB Type-C port, with USB 3.1 Gen 2 support</li><li>1 x USB 3.1 Gen 2 Type-A port (red)</li><li>1 x RJ-45 port</li><li>1 x optical S/PDIF Out connector</li><li>5 x audio jacks</li><li>Onboard USB: 1 x USB 3.1 Gen 1 header 2 x USB 2.0/1.1 headers</li><li>Form Factor: ATX</li><li>LED Lighting: RGB</li><li>Dimensions (W x L): 12.0' x 9.6'</li><li>Power Pin: 1 x 24-pin ATX main power connector 1 x 8-pin ATX 12V power connector</li></ul>";
-  productsModel.category = "Motherboard";
+    "<ul><li>Brand: Seagate</li><li>Specifications: 1 TB</li><li>Standard Model Number: ST1000DM010</li><li>Bytes per Sector: 4,096</li><li>Performance</li><li>Interface: SATA 6Gb/s</li><li>Transfer Rates Supported (Gb/s): 6.0/3.0/1.5</li><li>Max Sustained Data Rate OD Read (MB/s) s: 210MB/s</li><li>Cache, Multi-segmented (MB): 64 32</li><li>Rotational Speed (RPM): 7,200</li><li>Recording Technology: CMR</li><li>Reliability/Data Integrity</li><li>Load/Unload Cycles: 50,000</li><li>Non-recoverable Read Errors per Bits Read: Max - 1 per 10E14</li><li>Power-On Hours (per year): 2,400</li><li>Workload Rate Limit (TB/Year): 55</li><li>Warranty, Limited (years): 2</li><li>Power Management Startup Power (A): 2</li><li>Operating Mode, Typical (W): 5.3</li><li>Idling Average (W): 4.6 W</li><li>Standby Mode/Sleep Mode, Typical (W): 0.94/0.94</li><li>Voltage Tolerance, Inc. Noise: (5 V) ±5%</li><li>Voltage Tolerance, Inc. Noise: (12 V) ±10%</li><li>Environmental/Temperature Operating (ambient, min): 0°C</li><li>Operating (drive case, max): 60°C</li><li>Non-operating (ambient): 40°C – 70°C</li><li>Halogen-free: Yes</li><li>RoHS compliance: Yes</li><li>Physical</li><li>Height (mm/in, max): 19.99 mm/0.787 in</li><li>Width (mm/in, max): 101.85 mm/4.01 in</li><li>Depth (mm/in, max): 146.99 mm/5.787 in</li><li>Weight (g/lb, typical): 400 g/0.88 lb</li><li>Benefits:</li><li>Desktop or all-in-one PCs</li><li>Home servers</li><li>Entry-level direct-attached storage</li><li>devices (DAS)</li><li>Features:</li><li>Cost-effective storage upgrade for laptop or desktop computers</li><li>Store all your games, music, movies and more with up to 4TB of storage</li><li>SATA 6Gb/s interface optimizes burst performance; 64MB Cache</li><li>Seagate Secure models for hardware-based data security</li><li>Instant Secure Erase allows safe and easy drive retirement</li></ul>";
+  productsModel.category = "SSD/HDD";
 
   // Insert to DB
   productsModel.save((err, data) => {
@@ -101,11 +112,6 @@ exports.insert = (req, res) => {
 // exports.all = (req, res) => {
 //   res.send("hooray");
 // };
-
-// Categorize
-exports.categorize = (req, res) => {
-  res.send("hoorah");
-};
 
 // const mysql = require("mysql2");
 
